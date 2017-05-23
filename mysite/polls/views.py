e15a2de9a6c0e9669759339db372a0f5ffe3a536
Stefan Lehmann <stefan.st.lehmann@gmail.com>
2017-05-23 12:44:58 +0000

    init commit

---
+++ mysite/polls/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question}) 
+
+
+def results(request, question_id):
+    response = 'You are looking at the results of question {}'
+    return HttpResponse(response.format(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse('You are voting on question {}.'.format(question_id))
+
